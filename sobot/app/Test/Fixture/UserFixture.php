36b3e461ce0cbfa048d893dd2713a90a9ed29845
Ma Eikei <maeieki@gmail.com>
2014-04-28 12:55:12 +0900

    sqlite is right configured

---
+++ sobot/app/Test/Fixture/UserFixture.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * UserFixture
+ *
+ */
+class UserFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'length' => 11, 'key' => 'primary'),
+		'username' => array('type' => 'string', 'null' => false),
+		'password' => array('type' => 'string', 'null' => false, 'length' => 40),
+		'group_id' => array('type' => 'text', 'null' => false, 'length' => 11),
+		'created' => array('type' => 'datetime', 'null' => true),
+		'modified' => array('type' => 'datetime', 'null' => true),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'username', 'unique' => 1)
+		),
+		'tableParameters' => array()
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'username' => 'Lorem ipsum dolor sit amet',
+			'password' => 'Lorem ipsum dolor sit amet',
+			'group_id' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+			'created' => '2014-04-28 11:59:00',
+			'modified' => '2014-04-28 11:59:00'
+		),
+	);
+
+}
