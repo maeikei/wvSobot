ba1c5bc84b1a095ebe2f8306911b62cba9b36882
Ma Eikei <maeieki@gmail.com>
2014-04-30 20:55:46 +0900

    schemma change

--- sobot/app/Config/Schema/schema.php
+++ sobot/app/Config/Schema/schema.php
@@ -17,15 +17,29 @@ class AppSchema extends CakeSchema {
 		'tableParameters' => array()
 	);
 
+	public $twitterOAuth = array(
+		'id' => array('type' => 'integer', 'null' => false, 'length' => 11, 'key' => 'primary'),
+		'apiKey' => array('type' => 'text', 'null' => false),
+		'apiSecret' => array('type' => 'text', 'null' => false),
+		'accessToken' => array('type' => 'text', 'null' => false),
+		'accessTokenSecret' => array('type' => 'text', 'null' => false),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => 'CURRENT_TIMESTAMP'),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => 'CURRENT_TIMESTAMP'),
+		'indexes' => array(
+			
+		),
+		'tableParameters' => array()
+	);
+
 	public $users = array(
 		'id' => array('type' => 'integer', 'null' => false, 'length' => 11, 'key' => 'primary'),
 		'username' => array('type' => 'string', 'null' => false),
 		'password' => array('type' => 'string', 'null' => false, 'length' => 40),
 		'group_id' => array('type' => 'text', 'null' => false, 'length' => 11),
-		'created' => array('type' => 'datetime', 'null' => true),
-		'modified' => array('type' => 'datetime', 'null' => true),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'indexes' => array(
-			'PRIMARY' => array('column' => 'username', 'unique' => 1)
+			
 		),
 		'tableParameters' => array()
 	);
